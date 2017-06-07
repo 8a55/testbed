f64d20835727e222609c1a4877ce0faa8bfa744f
8a55 <8a55@mail.ru>
2017-06-07 15:44:55 +0300

    111

---
+++ unit1.pas
@@ -0,0 +1,45 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+ Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+ { TForm1 }
+
+ TForm1 = class(TForm)
+  Button1: TButton;
+  Memo1: TMemo;
+  procedure Button1Click(Sender: TObject);
+ private
+  { private declarations }
+ public
+  { public declarations }
+ end;
+
+var
+ Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Button1Click(Sender: TObject);
+var i:integer;
+begin
+  for i:=0 to 10 do begin
+    memo1.Lines.Add(inttostr(i));
+    if i=2 then break;
+  end;
+  memo1.Lines.Add('fin '+inttostr(i));
+
+end;
+
+end.
+
